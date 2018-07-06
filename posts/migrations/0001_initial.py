766cece6d1d3add9c177474047cccc65f4c25a3f
antihype <ryskulova_ai@auca.kg>
2018-07-06 14:23:04 +0600

    author with FK

--- posts/migrations/0001_initial.py
+++ posts/migrations/0001_initial.py
@@ -18,7 +18,6 @@ class Migration(migrations.Migration):
             name='Post',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('author', models.IntField(default = 2))
                 ('published', models.BooleanField(default=False)),
                 ('title', models.CharField(max_length=200)),
                 ('audience', models.Enum(default = 1)),
@@ -29,7 +28,7 @@ class Migration(migrations.Migration):
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('locale'), models.CharField(max_length=30),
                 ('language'),models.Enum(default = 1)
-                ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, unique=True)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL, unique=True)),
             ],
         ),
     ]
